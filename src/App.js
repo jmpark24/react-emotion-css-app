f33ecefa12885b03764ed19fe551c5bcdf2fcd85
jmpark <stylack@gamil.com>
2024-03-26 17:09:28 +0900

    Global Style 적용

--- src/App.js
+++ src/App.js
@@ -2,10 +2,29 @@ import { Outlet, Route, Routes } from 'react-router-dom';
 import logo from './logo.svg';
 import SearchPage from './pages/SearchPage';
 import BookDetailPage from './pages/BookDetailPage';
+import { Global, css } from '@emotion/react';
 
 const Layout = () => {
   return (
     <div>
+      <Global
+        styles={css`
+          body {
+            background-color: white;
+            color: block;
+            transition-duration 0.2s;
+            transition-property: background-color, color;
+          }
+          a {
+            color: black;
+            text-decoration: none;
+          }
+          ul {
+            list-style: none;
+            padding: 0;
+          }
+      `}
+      />
       <Outlet />
     </div>
   );
